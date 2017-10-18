d246fe82eeb7ff8747607b587071e6448f21f026
Martin Zilak <tinnyx@gmail.com>
2017-10-18 13:05:50 +0200

    implemented sellCar(Car car)

--- git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
+++ git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import cz.fi.muni.carshop.CarShopStorage;
 import cz.fi.muni.carshop.entities.Car;
 import cz.fi.muni.carshop.enums.CarTypes;
+import cz.fi.muni.carshop.exceptions.RequestedCarNotFoundException;
 
 public class CarShopStorageServiceImpl implements CarShopStorageService {
 
@@ -30,7 +31,22 @@ public class CarShopStorageServiceImpl implements CarShopStorageService {
 
 	@Override
 	public void addCarToStorage(Car car) {
+                if(car.getPrice() < 0) {
+                    throw new IllegalArgumentException("Car price can't be less than 0!");
+                }
+            
 		CarShopStorage.getInstancce().getCars().computeIfAbsent(car.getType(), x -> new ArrayList<>()).add(car);
 	}
+
+        @Override
+        public void sellCar(Car car) throws RequestedCarNotFoundException {
+            List<Car> cars = CarShopStorage.getInstancce().getCars().get(car.getType());
+                    
+            if (cars.contains(car)) {
+                cars.remove(car);
+            } else {
+                throw new RequestedCarNotFoundException("Can't sell a car that is not in storage!");
+            }
+        }
 
 }
